1abd1f5731f5e1be011ef616f6df9f624a00a0e2
Matheus Cardoso <matheus.silvacardoso10@gmail.com>
2021-01-17 14:51:37 -0300

    test database config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -11,9 +11,9 @@ export const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
-// if (!firebase.apps.length) {
-firebase.initializeApp(firebaseConfig)
-// }
-const database = firebase.database()
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+const database = process.browser ? firebase.database() : undefined
 
 export default database
